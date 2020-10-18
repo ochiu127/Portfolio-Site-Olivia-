215b0dc06588d90c8457ace8920884007f2ee5d7
ochiu127 <71469171+ochiu127@users.noreply.github.com>
2020-10-18 11:02:06 -0400

    toggle mobile navigation via js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Hellow Friend! Welcome to my page!")
+console.log("Hello Friend! Welcome to my page!");
+
+function menuToggle() {
+var x = document.getElementById('myNavtoggle');
+if (x.className === 'navtoggle') {
+    x.className += 'responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
