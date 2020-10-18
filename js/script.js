1b4b9a22d2e5d64399840222b9b9329348c473e1
ochiu127 <71469171+ochiu127@users.noreply.github.com>
2020-10-18 16:51:44 -0400

    corrected className responsive code

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ console.log("Hello Friend! Welcome to my page!");
 function menuToggle() {
 var x = document.getElementById('myNavtoggle');
 if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
     } else {
         x.className = 'navtoggle';
     }
